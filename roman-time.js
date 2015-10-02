72bd88033b635f224cd58ac117659c68eefda6c5
Данил Копорушкин <danilok97@gmail.com>
2015-10-02 23:20:55 +0500

    Данил Копорушкин

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,20 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
-
+var RomeHours;
+var RomeMinutes;
 // Немного замечательного кода и магии
+if (hours > 24 || hours < 0 || minutes > 59 || minutes < 0) {
+    console.log('Время указанно неверно')
+}
+else {
+
+    var Rome1to9 = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VII', 'IX'];
+    var Rome10 = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    RomeMinutes = Rome10[Math.floor(minutes / 10)] + Rome1to9[minutes % 10];
+    RomeHours = Rome10[Math.floor(hours / 10)] + Rome1to9[hours % 10];
+    if (hours == 0 || hours == 24) RomeHours = 'NN';
+    if (minutes == 0) RomeMinutes = 'NN';
+    var HexToRomConv = RomeHours + ' ' + ':' + ' ' + RomeMinutes;
+    console.log(HexToRomConv);
+}
+
